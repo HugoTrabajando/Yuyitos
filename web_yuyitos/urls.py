18606dc81289b2796702c9ac76893c3249d3b9a0
HugoTrabajando <82228255+HugoTrabajando@users.noreply.github.com>
2023-09-23 03:50:52 -0300

    al filo 2

---
+++ web_yuyitos/urls.py
@@ -0,0 +1,23 @@
+###este archivo tiene todas las rutas de la pagina web
+##es necesario. Fue copiado del proyecto_toreto/urls.py
+
+from django.contrib import admin
+from django.urls import include, path
+##escribir un from de las views.py
+from .views import cerrar_sesion, index, logear, registro, test
+
+###rutas redireccionar las paginas
+urlpatterns = [
+    path('test', test,name='TEST'), ##pagina testing
+    path('', index,name='INDEX'), ##pagina index si no se escribe nada
+
+    path('logear/',logear,name="LOGEAR"),
+    path('registro/',registro,name="REGISTRO"),
+    path('cerrar/',cerrar_sesion,name='CERRAR'),
+
+    path('registrar_cliente/',registrar_cliente,name='REGISTRAR_CLIENTE'),
+    path('modificar_cliente/',modificar_cliente,name='MODIFICAR_CLIENTE'),
+    path('modificar_cliente/<id_client>/',reg_cli_modificar,name='MODI_CLIENTE'),
+    path('eliminar_cliente/<id_client>/',reg_cli_eliminar,name='ELIMINAR_CLIENTE'),
+
+]
--- web_yuyitos/views.py
+++ web_yuyitos/views.py
@@ -1,3 +1,82 @@
+from django.contrib import auth
+from django.db.models.fields.mixins import FieldCacheMixin
+from django.db import reset_queries
+from django.http import response
+from datetime import datetime
 from django.shortcuts import render
+from .models import Tecnico, Cliente, Repuesto, Proveedor, Atencion_hora, Servicio, Order_repuesto, Galeria, Vehiculo
+# importar el modelo de usuarios
+from django.contrib.auth.models import User
+# importar librerias de acceso a login
+from django.contrib.auth import authenticate,logout,login as login_aut
+# importar libreria de decorador para impedir el acceso a las paginas
+from django.contrib.auth.decorators import login_required,permission_required
+
 
 # Create your views here.
+####segun, se debe crear metodos para asi funcionen en urls.py
+####debo renderizar paginas, creando metodos aca
+
+###testing
+def test(request):
+    return render(request, "./test.html")
+###fin testing
+
+def index(request):
+    return render(request, "./index.html")
+
+def logear(request):
+    if request.POST:
+        usuario = request.POST.get("txtuser")
+        contra = request.POST.get("txtcontrasena")
+        us = authenticate(request,username=usuario,password=contra)
+        if us is not None and us.is_active:
+            login_aut(request,us)
+            tipos = Tecnico.objects.all()
+            contexto ={"tipos":tipos}
+            return render(request, "index.html",contexto)          
+        else:
+            contexto = {"mensaje":"Usuario o contraseña incorrecto"}
+            return render(request,"login.html",contexto)
+    return render(request,"login.html")
+
+##REGISTRO / SIGN IN DE PAGINA
+def registro(request):
+    ##return render(request,"registro.html")
+    mensaje=""
+    if request.POST:
+        nombre = request.POST.get("txtnombre")
+        nom_usuario = request.POST.get("txtusuario")
+        email = request.POST.get("txtcorreo")
+        pass1 = request.POST.get("txtcontrasena")
+        passok = request.POST.get("txtcontrasenaok")
+
+        if pass1!=passok:
+            mensaje="contraseñas no son iguales"
+            contexto= {"mensaje":mensaje}
+            return render(request,"registro.html",contexto)        
+
+        usu = User()
+        usu.first_name = nombre
+        ##--usu.last_name = apellido
+        usu.email = email
+        usu.username = nom_usuario
+        usu.set_password(pass1)
+        usu.save()
+        mensaje="Se ha guardado esta creación de usuario!"
+
+    contexto={"mensaje":mensaje}
+    return render(request,"registro.html",contexto)
+
+##cierre de sesion
+def cerrar_sesion(request):
+    logout(request)
+    tipos = Tecnico.objects.all()
+    contexto ={"tipos":tipos}
+    return render(request, "index.html",contexto)
+
+##exclusivo solo para logeados
+##Esto cada vez que es escrito, afecta al DEF de abajo, no de arriba....
+######estas variables pueden servir ante emergencia de errores
+###usuario1 = User().username
+fecha_ahora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
