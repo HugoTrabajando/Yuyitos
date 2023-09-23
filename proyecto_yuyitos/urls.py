8e22a55a24cffdf6871349d02b26c5fde882a455
HugoTrabajando <82228255+HugoTrabajando@users.noreply.github.com>
2023-09-23 02:29:02 -0300

    al filo

--- proyecto_yuyitos/urls.py
+++ proyecto_yuyitos/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('web_yuyitos.urls')) ##recupera rutas de yuyitos web
 ]
+
+##incluir en path, la direccion y nombre de directorio 'MEDIA'!
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+###NOMBRE DE PAGINA 
+admin.site.site_header = 'Administración de almacen Los Yuyitos'
