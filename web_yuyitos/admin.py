2a3de973b13125c200e37f5059afc581606de9f3
HugoTrabajando <82228255+HugoTrabajando@users.noreply.github.com>
2023-09-24 23:05:32 -0300

    al filo 4

--- web_yuyitos/admin.py
+++ web_yuyitos/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from .models import Cliente, Proveedor, Producto, Abono, Venta, DetalleVenta
 
 # Register your models here.
-from .models import Cliente, Proveedor, 
+admin.site.register(Cliente)
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Abono)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
